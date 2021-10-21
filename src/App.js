0d5ebbcc4faa99e301a8510f5fa8497e92e98447
Giulia Amaral <75877954+GiuliaAmaral@users.noreply.github.com>
2021-10-20 22:15:19 -0300

    up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+
+  const [carregando, setCarregando] = useState(true);
+
+  // Função do react que é executada sempre que o componente é carregando
+  useEffect(() => {
+
+    setCarregando(false);
+
+  }, [])
+
+
+  return (<>
+    {
+      carregando ? (<>
+        <h1>Carregando...</h1>
+      </>) : (<div className="fundoNuvem">
+
+        <nav class="navbar navbar-light bg-light">
+          <div class="container-fluid">
+            <a class="navbar-brand" href="#"><i class="bi bi-plus-lg" style={{fontSize: "1.5em"}}></i></a>
+          </div>
+        </nav>
+
+      </div>)
+    }
+
+  </>);
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import 'jquery/dist/jquery.js';
+import 'bootstrap/dist/css/bootstrap.css';
+import "bootstrap-icons/font/bootstrap-icons.css";
+import 'bootstrap/dist/js/bootstrap.js';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
